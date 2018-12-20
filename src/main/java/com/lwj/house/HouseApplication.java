dc68c0361c9b1c46e4364fc5f4ae66d7b378b62f
lwj <864391385@qq.com>
2018-12-20 12:58:26 +0800

    Initial commit

---
+++ src/main/java/com/lwj/house/HouseApplication.java
@@ -0,0 +1,17 @@
+package com.lwj.house;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Administrator
+ */
+@SpringBootApplication
+public class HouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HouseApplication.class, args);
+    }
+
+}
+
