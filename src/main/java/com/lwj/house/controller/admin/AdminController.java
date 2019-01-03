afede2de7c25996104c16f5f5100a432e8578afd
lwj <864391385@qq.com>
2019-01-03 12:49:28 +0800

    update：上传图片到本地

--- src/main/java/com/lwj/house/controller/admin/AdminController.java
+++ src/main/java/com/lwj/house/controller/admin/AdminController.java
@@ -1,8 +1,13 @@
 package com.lwj.house.controller.admin;
 
+import com.lwj.house.base.ApiResponse;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * @author lwj
@@ -30,4 +35,24 @@ public class AdminController {
     public String add(){
         return "admin/house-add";
     }
+
+    @PostMapping(value = "/upload/photo", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @ResponseBody
+    public ApiResponse uploadPhoto(@RequestParam("file") MultipartFile file) {
+        if (file == null) {
+            return ApiResponse.ofStatus(ApiResponse.Status.NOT_VALID_PARAM);
+        }
+        String fileName = file.getOriginalFilename();
+        File target = new File("E:/workSpace/house/tmp/" + fileName);
+        try {
+            file.transferTo(target);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ApiResponse.ofStatus(ApiResponse.Status.INTERNAL_SERVER_ERROR);
+        }
+
+
+        return ApiResponse.ofSuccess(null);
+    }
+
 }
